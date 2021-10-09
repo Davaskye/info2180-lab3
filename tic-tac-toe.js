0988f182fd66c90d735858450e2cb2b032d077ce
Davaskye DeBique <Davaskye@gmail.com>
2021-10-08 22:37:46 -0400

    Completed win checking functionality.

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,12 +2,15 @@ window.onload = function() {
     let boxes = document.getElementById("board").children;
     for (let i = 0; i < boxes.length; i++){
         boxes[i].classList.add("square");
+        boxes[i].setAttribute("id", i);
         boxes[i].onclick = function(){
             clickTracker(boxes[i]);
         }
+
         boxes[i].onmouseover = function(){
             hoverTracker(boxes[i]);
         }
+
         boxes[i].onmouseout = function(){
             hoverTrackerRemoval(boxes[i]);
         }
@@ -16,8 +19,17 @@ window.onload = function() {
     
 }
 
-let whichOne = 0 //if value is 0 - player is X AND if value is 1 - player is O.
-const currentPlays = []; //keeps track of what was played
+
+let gameOver = false; // this variable will handle if the game is over or not
+let whichOne = 0; //if value is 0 - player is X AND if value is 1 - player is O.
+const winningSquares = [
+    [0,1,2], [3,4,5], [6,7,8],
+    [0,3,6], [1,4,7], [2,5,8],
+    [0,4,8], [2,4,6] 
+];
+
+console.log(winningSquares.includes([0,1,2]));
+const currentPlays = ["","","","","","","","",""]; //keeps track of what was played
 
 let clickTracker = function(currentBox){
       /*  let option = document.querySelector("square");*/
@@ -27,22 +39,58 @@ let clickTracker = function(currentBox){
         whichOne = 1;
         currentBox.innerHTML = "X";
         currentBox.classList.add("X");
-        currentPlays.push(player);
+        currentPlays[currentBox.id] = "X";
     }
     else{
         player = "O";
         whichOne = 0;
         currentBox.innerHTML = "O";
         currentBox.classList.add("O");
-        currentPlays.push(player);
+        currentPlays[currentBox.id] = "O";
 
     }
-    console.log(`${player} clicked here.`);
+    winChecker();
+    console.log(`${player} clicked at square ${currentBox.id}.`);
+    console.log(currentPlays);
 }
 
+//This section deals with win checking
+function winChecker () {
+    for (let i = 0; i < 8; i++){
+        let winCheck = winningSquares[i];
+        let a = currentPlays[winCheck[0]];
+        let b = currentPlays[winCheck[1]];
+        let c = currentPlays[winCheck[2]];
+
+        if (a == "" || b == "" || c==""){
+            continue;
+        }
+        if (a == b && b ==c){
+            gameOver = true;
+            break;
+        }
+    }
+    if (gameOver){
+        let statusBox = document.getElementById("status");
+        let winnerMsg = `Congratulations! ${player} is the Winner!`;
+        statusBox.classList.add("you-won");
+        statusBox.innerHTML = winnerMsg;
+        console.log("Game Over Bro!!");
+    }
+}
+
+
+//This section deals with hover tracking
 let hoverTracker = function(currentBox){
     currentBox.classList.add("hover");
 }
+
 let hoverTrackerRemoval = function(currentBox){
     currentBox.classList.remove("hover");
 }
+
+//TO DO LIST BELOW:
+//fill array in such a way that it would match winning numbers.
+/*  for example, if index 2 is selected, fill index 2 with value whether X or Y. 
+*/
+//can removeDefault for stuff already filled.
